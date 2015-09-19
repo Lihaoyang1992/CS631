4c063bc7f229258f0fc3d06f7ab41dcd569e1c8a
李昊旸 <lee@lihaoyangdeMBP.home>
2015-09-18 23:26:07 -0400

    set child wait parent

--- createChild2.c
+++ createChild2.c
@@ -19,6 +19,21 @@ int main() {
 		perror("Error: can't create a child process");
 		return 1;
 	} else if (pid == 0) {
+		// wait parents to set read fifo: read only, so close the write-descriptor
+		w = waitpid(getppid(), &status, WUNTRACED | WCONTINUED);
+		if (w == -1) {
+			perror("Error: can't wait child");
+		}
+		if (WIFEXITED(status)) {
+			printf("Child exited status = %d\n", WEXITSTATUS(status));
+		} else if (WIFSIGNALED(status)) {
+			printf("killed by sigal %d\n", WTERMSIG(status));
+		} else if (WIFSTOPPED(status)) {
+			printf("stopped by signal %d\n", WSTOPSIG(status));
+		} else if (WIFCONTINUED(status)) {
+			printf("continued\n");
+		}
+
 		// child: write only, so close the read-descriptor
 		while (userInput > 255 || userInput < 0) {
 			printf("Please input a valid integer (0 ~ 255): ");
@@ -33,21 +48,8 @@ int main() {
 		// remove the FIFO
 		unlink(myfifo);
 	} else {
-		// parent: read only, so close the write-descriptor
-		w = waitpid(pid, &status, WUNTRACED | WCONTINUED);
-		if (w == -1) {
-			perror("Error: can't wait child");
-		}
-		if (WIFEXITED(status)) {
-			printf("Child exited status = %d\n", WEXITSTATUS(status));
-		} else if (WIFSIGNALED(status)) {
-			printf("killed by sigal %d\n", WTERMSIG(status));
-		} else if (WIFSTOPPED(status)) {
-			printf("stopped by signal %d\n", WSTOPSIG(status));
-		} else if (WIFCONTINUED(status)) {
-			printf("continued\n");
-		}
-		if ((fd = open(myfifo, O_RDONLY)) < 0)
+		// parent to read
+		if ((fd = open(myfifo, O_RDONLY | O_NONBLOCK)) < 0)
 			perror("Error: fifo error");
 		read(fd, &userInput, sizeof(userInput));
 		printf("Hi, I'm parent, my child %d just pass me %d\n", pid, userInput);
