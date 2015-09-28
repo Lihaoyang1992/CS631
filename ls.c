69ce5d7b4e0a12f03349a4535a6ad199b9c8be64
李昊旸 <lee@host-studentw-142-5.dhcp.stevens-tech.edu>
2015-09-27 20:43:03 -0400

    fixed bugs

--- ls.c
+++ ls.c
@@ -72,12 +72,12 @@ void show_file_info(char* filename, struct stat * info_p) {
 	mode_to_letter(info_p->st_mode, modestr);
 
 	printf("%s", modestr);
-	printf("%4d ", (int)info_p->st_nlink);
-	printf("%-8s ", uid_to_name(info_p->st_uid));
-	printf("%-8s ", gid_to_name(info_p->st_gid));
-	printf("%8ld ", (long)info_p->st_size);
-	printf("%.12s ", 4 + ctime(&info_p->st_mtime));
-	printf("%s\n ", filename);
+	printf("%4d", (int)info_p->st_nlink);
+	printf("%-8s", uid_to_name(info_p->st_uid));
+	printf("%-8s", gid_to_name(info_p->st_gid));
+	printf("%8ld", (long)info_p->st_size);
+	printf("%.12s", 4 + ctime(&info_p->st_mtime));
+	printf("%s\n", filename);
 }
 
 void do_stat(char* filename)
